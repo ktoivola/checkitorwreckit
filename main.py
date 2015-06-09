5e20e65cc0523370c523208093e3e4259325d26e
ktoivola <ktoivola@miasole.com>
2015-06-09 15:34:26 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
+
 from openpyxl import load_workbook
-wb = load_workbook(filename = 'test.xlsx')
+
+wb = load_workbook(filename = 'Test excel.xlsx')  
 sheet1 = wb['Sheet1']
-last = 'A1'
+file_name = 'Test excel.xlsx'
+
+sheet2 = wb.create_sheet(title="copy")
+rowcount = 0
+colcount = 0
+
+last = 'A1'  
 for row in sheet1.rows:
+    rowcount += 1
+    colcount = 0
+    print (rowcount)
     for col in row:
-        print col.value
+        colcount += 1
+        print (colcount)
+        sheet2.cell(column=colcount, row=rowcount).value = col.value
+  
+wb.save(filename = file_name)
+
+
